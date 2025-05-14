9300a72f40b35f30cab944d8e850547725be181a
Zoran <zoran.trpevski@gmail.com>
2025-05-14 13:38:19 +0200

    Delete avahi.sh

--- avahi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Start avahi-daemon in the background
-service dbus start
-avahi-daemon --daemonize
-
-# Start shairport-sync in foreground
-exec shairport-sync -c /etc/shairport-sync.conf -vv
