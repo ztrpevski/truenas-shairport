ab4a4d4d041f39da9d2ab080fa8209504443e1ba
Zoran <zoran.trpevski@gmail.com>
2025-06-19 23:02:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@
 # Start avahi-daemon in the background
 service dbus start
 avahi-daemon --daemonize
-
 # Start shairport-sync in foreground
 exec shairport-sync -c /etc/shairport-sync.conf -vv && bash
